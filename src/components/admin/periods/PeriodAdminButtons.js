de22e29080e41838a764f7df5986b801a73e522b
Your Name <you@example.com>
2022-10-24 20:08:28 +0000

    2410

--- src/components/admin/periods/PeriodAdminButtons.js
+++ src/components/admin/periods/PeriodAdminButtons.js
@@ -1,13 +1,13 @@
 /** @format */
 
 import React from 'react';
-import {Button, Grid} from '@mui/material';
+import { Button, Grid } from '@mui/material';
 
 const PeriodAdminButtons = () => {
 	return (
 		<Grid container={true} columns={4} mb={2}>
 			<Grid item xs={1}>
-				<Button>Button</Button>
+				<Button color='action'>Increment Period</Button>
 			</Grid>
 			<Grid item xs={1}>
 				<Button>Button</Button>
